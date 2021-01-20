777c5a195dc02d9c292c622930a4cb132e689e59
DESKTOP-T5S11OF\Andreas <schoen-andreas@web.de>
2021-01-20 20:07:40 +0100

    init

---
+++ src/main/java/com/aschoen/msdrinkservice/MsDrinkServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aschoen.msdrinkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDrinkServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsDrinkServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aschoen/msdrinkservice/MsDrinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aschoen.msdrinkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDrinkServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
